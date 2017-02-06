b6993333c881a557f222f31e5e51847bf8806986
ramon dubbink <ramon dubbink>
2017-02-06 09:52:30 +0100

    Formulier toegevoegd

---
+++ index.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<HTML>
+<HEAD>
+<META content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<LINK href="Homepage.css" rel="stylesheet" type="text/css" />
+<link href="formulier.php">
+<TITLE>Hoofdman</TITLE>
+</HEAD>
+
+    <BODY>
+    <H1>Hey sybje schat</H1>
+            
+            <form action="" method="post">
+            Naam: <input type="text" name="naam" id="naam"><br>
+            Kleur: <input type="text" name="kleur" id="kleur"><br>
+            <input type="submit" name="submit" id="submit">
+    
+<?php
+require "formulier.php";
+?>
+</form>
+    
+    </BODY>
+</HTML>
+<!--
+
+                        <!DOCTYPE html>
+<HTML>
+<HEAD>
+<META content="text/html; charset=utf-8" http-equiv="content-Type" />
+<LINK href="formulier.php">
+<TITLE>Basiscursus HTML5</TITLE>
+</HEAD>
+<BODY>
+
+<h1> Hello World </h1>
+
+<form method="POST"
+	action="formulier.php">
+Naam:<input type="text" name="naam" id="naam"><br>
+Kleur:<input type="text" name="kleur" id="kleur">
+<input type="submit" value="klik me :D" name="submit" id="submit">
+
+
+
+</form>
+</body>
+</html>
+-->
